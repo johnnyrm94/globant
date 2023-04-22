0a1b459f7e732cc1999f33d7aa58ee33d1e0f96d
johnnyrm94 <johnnyrm94@gmail.com>
2023-04-22 12:23:06 -0600

    Finishing point 1

--- Challenge1/CSV_to_DB.py
+++ Challenge1/CSV_to_DB.py
@@ -1,27 +1,21 @@
 import pandas as pd
 from sqlalchemy import create_engine
-import sqlalchemy
+from sqlalchemy.types import Integer
+from sqlalchemy.types import String
 
-DeparDF = pd.read_csv('departments.csv')
-HiredDF2 = pd.read_csv('hired_employees.csv')
-JobsDF3 = pd.read_csv('jobs.csv')
-
-#print(JobsDF3.to_string()) 
-
-#print(HiredDF2.to_string()) 
-
-#print(DeparDF.to_string()) 
+DeparDF = pd.read_csv('departments.csv', header=None, names=['id', 'department'])
+HiredDF2 = pd.read_csv('hired_employees.csv', header=None, names=['id', 'name', 'datetime', 'department_id', 'job_id'])
+JobsDF3 = pd.read_csv('jobs.csv', header=None, names=['id', 'job'])
 
 engine = create_engine('postgresql://postgres:Nasus1994.@globantdb.c1fhwpqnf58i.us-east-1.rds.amazonaws.com:5432/globantdb')
 
-dt = {'id': sqlalchemy.types.INTEGER(), 'department': sqlalchemy.types.String(length=20)}
+dt = {'id': Integer(), 'department': String(50)}
 DeparDF.to_sql('departments', con=engine, if_exists='append', index=False, dtype=dt) #columns: id and department
 
-
-dt = {'id': sqlalchemy.types.INTEGER(), 'name': sqlalchemy.types.String(length=8),'datetime': sqlalchemy.types.String(length=20), 'department_id': sqlalchemy.types.INTEGER(), 'job_id': sqlalchemy.types.INTEGER()} 
+dt = {'id': Integer(), 'name': String(50),'datetime': String(50), 'department_id': Integer(), 'job_id': Integer()} 
 HiredDF2.to_sql('hired_employees', con=engine, if_exists='append', index=False, dtype=dt) #columns: id, name, datetime, department_id, job_id
 
-dt = {'id': sqlalchemy.types.INTEGER(), 'job': sqlalchemy.types.String(length=20)}
+dt = {'id': Integer(), 'job': String(50)}
 JobsDF3.to_sql('jobs', con=engine, if_exists='append', index=False, dtype=dt) #columns: id, job 
 
 
