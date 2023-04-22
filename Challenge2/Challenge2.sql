58e1e84ba583373ac41bb56e37e5be57a87a5d64
johnnyrm94 <johnnyrm94@gmail.com>
2023-04-22 06:08:20 -0600

    Adding query 2

--- Challenge2/Challenge2.sql
+++ Challenge2/Challenge2.sql
@@ -15,3 +15,21 @@ order by d.department,j.job asc ;
 
 --Query 2
 
+with amount_hired as (
+	select count(id) amount, department_id
+	from hired_employees
+	where EXTRACT(YEAR FROM TO_TIMESTAMP(datetime, 'YYYY-MM-DD"T"HH24:MI:SS"Z"')) = 2021
+	group by department_id
+), 
+
+avg_hired as (
+	select AVG(amount) average
+	from amount_hired
+)
+
+select am.amount, am.department_id, d.department
+from amount_hired am 
+inner join avg_hired av on am.amount > av.average
+inner join departments d  on am.department_id = d.id
+order by am.amount desc;
+
